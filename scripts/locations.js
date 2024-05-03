3c2e9b6f24e3c9a86eb1cfa689640b3d4e14574a
Chelsea Brooks <cbrooks6212@gmail.com>
2024-05-03 10:41:42 -0400

    updateagain

--- scripts/locations.js
+++ scripts/locations.js
@@ -4,3 +4,12 @@ const guests =getGuests()
 const services = getServices()
 const attractions = getAttractions()
 
+
+export const locations = () => {
+    let locationsHTML = "<ol>";
+    for (const location of locations) {
+        locationsHTML += `<li data-id="${location.id}">${location.name}</li>`
+        }
+       locationsHTML += "</ol>"
+        return locationsHTML
+};
